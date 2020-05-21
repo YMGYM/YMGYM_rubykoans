99282890048f12ce4b0209c4bcf1031df130329a
YMGYM <good_day1234@naver.com>
2020-05-21 06:48:59 +0000

    20200521

--- index.rb
+++ index.rb
@@ -1,1 +1,30 @@
-puts "Hello World!"
+class Animal
+	LEGS = 4
+	def legs_in_animal
+	  LEGS
+	end
+end
+
+class Reptile < Animal
+	def legs_in_reptile
+	  LEGS
+	end
+end
+
+class MyAnimals
+	LEGS = 2
+
+	class Bird < Animal
+	  def legs_in_bird
+		LEGS
+	  end
+	end
+end
+
+class MyAnimals::Oyster < Animal
+	def legs_in_oyster
+	  LEGS
+	end
+end
+
+puts Oyster.new.kind_of?(MyAnimals)
