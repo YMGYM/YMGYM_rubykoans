50afb7cc8311037d74ba61ce7fd5271ccd8c7d66
YMGYM <good_day1234@naver.com>
2020-05-25 13:10:18 +0000

    20200525

--- koans/about_exceptions.rb
+++ koans/about_exceptions.rb
@@ -6,10 +6,10 @@ class AboutExceptions < Neo::Koan
   end
 
   def test_exceptions_inherit_from_Exception
-    assert_equal __, MySpecialError.ancestors[1]
-    assert_equal __, MySpecialError.ancestors[2]
-    assert_equal __, MySpecialError.ancestors[3]
-    assert_equal __, MySpecialError.ancestors[4]
+    assert_equal RuntimeError, MySpecialError.ancestors[1]
+    assert_equal StandardError, MySpecialError.ancestors[2]
+    assert_equal Exception, MySpecialError.ancestors[3]
+    assert_equal Object, MySpecialError.ancestors[4]
   end
 
   def test_rescue_clause
