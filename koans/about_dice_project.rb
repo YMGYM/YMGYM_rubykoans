1ac9a72346c73c8b922f4e530afcfb18674c2dae
YMGYM <good_day1234@naver.com>
2020-06-01 06:54:48 +0000

    20200601

--- koans/about_dice_project.rb
+++ koans/about_dice_project.rb
@@ -2,9 +2,20 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 # Implement a DiceSet Class here:
 #
-# class DiceSet
-#   code ...
-# end
+class DiceSet
+  attr_reader :values
+	def roll(count)
+		tmp_values = @values
+		@values = []
+		begin
+			count.times do
+				@values << (rand(6) + 1)
+				# 0~5 까지의 랜덤한 수를 생성한 후 1을 더함
+			end
+			condition = (tmp_values == @values)
+		end while condition
+	end
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
